618d739fd9f7126e63de5e8e253b30cfe647e592
Ayra125 <ayrathan.jegasothy@gmail.com>
2026-05-05 17:58:13 -0400

    feat: add mpu6050 header file

--- firmware/Peripherals/i2c/mpu6050.h
+++ firmware/Peripherals/i2c/mpu6050.h
@@ -1,1 +1,17 @@
+#include "stm32f4xx_hal.h"
 #define MPU6050_ADDR 0x68
+#define MPU6050_PWR_MGMT_1 0x6B
+#define MPU6050_ACCEL_XOUT_H 0x3B
+#define MPU6050_ACCEL_YOUT_H 0x3D
+#define MPU6050_ACCEL_ZOUT_H 0x3F
+#define MPU6050_GYRO_XOUT_H  0x43
+#define MPU6050_GYRO_YOUT_H  0x45
+#define MPU6050_GYRO_ZOUT_H  0x47
+
+HAL_StatusTypeDef MPU6050_Init(I2C_HandleTypeDef *hi2c);
+
+HAL_StatusTypeDef MPU6050_ReadAccel(I2C_HandleTypeDef *hi2c, int16_t *x, int16_t *y, int16_t *z);
+
+HAL_StatusTypeDef MPU6050_ReadGYRO(I2C_HandleTypeDef *hi2c, int16_t *x, int16_t *y, int16_t *z);
+
+

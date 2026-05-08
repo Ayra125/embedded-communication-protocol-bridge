5a022b20af517961a2dd5ab5386362cf1129cfa4
Ayra125 <ayrathan.jegasothy@gmail.com>
2026-05-07 23:28:04 -0400

    feat: implement SSD1306 Clear function

--- firmware/Peripherals/i2c/ssd1306.c
+++ firmware/Peripherals/i2c/ssd1306.c
@@ -1,12 +1,24 @@
 #include "SSD1306_H"
 #include <string.h>
 HAL_StatusTypeDef SSD1306_Init(I2C_HandleTypeDef *hi2c){
-if(SSD1306(*hi2c,SSD1306_SET_MUX_RATIO)!=HAL_OK) return HAL_ERROR;
-if(SSD1306(*hi2c,0x3F)!=HAL_OK) return HAL_ERROR;
-if(SSD1306(*hi2c,0xD3)!=HAL_OK) return HAL_ERROR;
-if(SSD1306(*hi2c,0xA8)!=HAL_OK) return HAL_ERROR;
-if(SSD1306(*hi2c,0x00)!=HAL_OK) return HAL_ERROR;
-if(SSD1306(*hi2c,0x40)!=HAL_OK) return HAL_ERROR;
+if(SSD1306_SendCommand(hi2c,SSD1306_SET_MUX_RATIO)!=HAL_OK) return HAL_ERROR;
+if(SSD1306_SendCommand(hi2c,0x3F)!=HAL_OK) return HAL_ERROR;
+if(SSD1306_SendCommand(hi2c,SSD1306_SET_DISP_OFFSET)!=HAL_OK) return HAL_ERROR;
+if(SSD1306_SendCommand(hi2c,0x00)!=HAL_OK) return HAL_ERROR;
+if(SSD1306_SendCommand(hi2c,SSD1306_SET_DISP_START)!=HAL_OK) return HAL_ERROR;
+if(SSD1306_SendCommand(hi2c,SSD1306_SEG_REMAP_NORMAL)!=HAL_OK) return HAL_ERROR;
+if(SSD1306_SendCommand(hi2c,SSD1306_COM_SCAN_NORMAL)!=HAL_OK) return HAL_ERROR;
+if(SSD1306_SendCommand(hi2c,SSD1306_SET_COM_PINS)!=HAL_OK) return HAL_ERROR;
+if(SSD1306_SendCommand(hi2c,0x02)!=HAL_OK) return HAL_ERROR;
+if(SSD1306_SendCommand(hi2c,SSD1306_SET_CONTRAST)!=HAL_OK) return HAL_ERROR;
+if(SSD1306_SendCommand(hi2c,0x7F)!=HAL_OK) return HAL_ERROR;
+if(SSD1306_SendCommand(hi2c,SSD1306_DISPLAY_RAM)!=HAL_OK) return HAL_ERROR;
+if(SSD1306_SendCommand(hi2c,SSD1306_NORMAL_DISPLAY)!=HAL_OK) return HAL_ERROR;
+if(SSD1306_SendCommand(hi2c,SSD1306_SET_CLK_DIV)!=HAL_OK) return HAL_ERROR;
+if(SSD1306_SendCommand(hi2c,0x80)!=HAL_OK) return HAL_ERROR;
+if(SSD1306_SendCommand(hi2c,SSD1306_SET_CHARGE_PUMP)!=HAL_OK) return HAL_ERROR;
+if(SSD1306_SendCommand(hi2c,SSD1306_CHARGE_PUMP_ON)!=HAL_OK) return HAL_ERROR;
+if(SSD1306_SendCommand(hi2c,SSD1306_DISP_ON)!=HAL_OK) return HAL_ERROR;
 return HAL_OK;
 }
 
@@ -24,8 +36,15 @@ HAL_StatusTypeDef SSD1306_SendData(I2C_HandleTypeDef *hi2c,uint8_t *data,uint16_
 }
 
 HAL_StatusTypeDef SSD1306_Clear(I2C_HandleTypeDef *hi2c){
-
-
+    uint8_t zeros[1024];
+    if(SSD1306_SendCommand(hi2c, SSD1306_COL_ADDR)!= HAL_OK) return HAL_ERROR;
+    if(SSD1306_SendCommand(hi2c, 0)!= HAL_OK) return HAL_ERROR;
+    if(SSD1306_SendCommand(hi2c, 127)!= HAL_OK) return HAL_ERROR;
+    if( SSD1306_SendCommand(hi2c, SSD1306_PAGE_ADDR)!= HAL_OK) return HAL_ERROR;
+    if(SSD1306_SendCommand(hi2c, 0)!= HAL_OK) return HAL_ERROR;
+    if(SSD1306_SendCommand(hi2c, 7) != HAL_OK) return HAL_ERROR;  
+    memset(zeros, 0, sizeof(zeros));
+    return SSD1306_SendData(hi2c,zeros,1024);
 }
 
 HAL_StatusTypeDef SSD1306_WriteString(I2C_HandleTypeDef *hi2c,char *string){
